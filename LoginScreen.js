382e48e5ef07bb1d9654f5a9d7a285ca15b0462b
yunyeonghun <102847042+yunyeonghun@users.noreply.github.com>
2022-05-20 22:09:00 +0900

    Update LoginScreen.js

--- LoginScreen.js
+++ LoginScreen.js
@@ -43,4 +43,65 @@ const [userId, setUserId] = useState('');
               setLoading(false);
               console.error(error);
             });
+            <Image
+            source={require('../src/Pet-logo.png')}
+            style={{
+              width: '55%',
+              height: 100,
+              resizeMode: 'contain',
+              margin: 30,
+            }}
+          /> 
+          const styles = StyleSheet.create({
+            mainBody: {
+              flex: 1,
+              justifyContent: 'center',
+              backgroundColor: '#f8f8d9',
+              alignContent: 'center',
+            },
+          
+            buttonStyle: {
+              backgroundColor: '#50611c',
+              borderWidth: 0,
+              color: '#FFFFFF',
+              borderColor: '#50611c',
+              height: 40,
+              alignItems: 'center',
+              borderRadius: 30,
+              marginLeft: 35,
+              marginRight: 35,
+              marginTop: 20,
+              marginBottom: 25,
+            },
+            buttonTextStyle: {
+              color: '#647a23',
+              paddingVertical: 10,
+              fontSize: 16,
+            },
+            inputStyle: {
+              flex: 1,
+              color: 'Dark green',
+              paddingLeft: 15,
+              paddingRight: 15,
+              borderWidth: 1,
+              borderRadius: 30,
+              borderColor: '#647a23',
+            },
+            registerTextStyle: {
+              color: '#647a23',
+              textAlign: 'center',
+              fontWeight: 'bold',
+              fontSize: 14,
+              alignSelf: 'center',
+              padding: 10,
+            },
+            errorTextStyle: {
+              color: 'red',
+              textAlign: 'center',
+              fontSize: 14,
+            },
+          });
+          
+           
+        
             
