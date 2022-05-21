441b7802cf6884e00a0e0249e1f1e1f5055b8a9b
yunyeonghun <102847042+yunyeonghun@users.noreply.github.com>
2022-05-21 14:02:06 +0900

    App.js

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>I'm apple</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f8f8d9',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
